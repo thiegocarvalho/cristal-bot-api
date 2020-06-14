c45ef90ace6a52280e612b4c3ebabb61ed116974
Thiego Carvalho <thiego@rocketmail.com>
2020-06-14 20:23:19 -0300

    Cheguei

--- routes/botman.php
+++ routes/botman.php
@@ -3,6 +3,10 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
+$botman->hears('cheguei', function ($bot) {
+    $bot->reply('Que bom que você chegou bem, veja a sua pontuação https://cristal.netlify.app/duration');
+});
+
 $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
