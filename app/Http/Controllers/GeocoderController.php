edb8887057b0acafc5763c9b515841e6fa4b5264
Thiego Carvalho <thiego@rocketmail.com>
2020-06-14 18:36:37 -0300

    Deploy Heroku

--- app/Http/Controllers/GeocoderController.php
+++ app/Http/Controllers/GeocoderController.php
@@ -9,15 +9,21 @@ class GeocoderController extends Controller
 {
     public function getAddressGeocode(Request $request)
     {
-
         if($request->address){
             $client = new Client();
             $res= $client->get(
                 'https://geocode.search.hereapi.com/v1/geocode?q='.$request->address.' &apiKey='.env('HERE_API_KEY')
             );
 
-            return response()->json(json_decode(($res->getBody())->getContents()));
+            return response()->json(
+                json_decode(
+                    ($res->getBody())->getContents()
+                )
+            );
         }
+        return response()->json(
+            'No address request', 400
+        );
     }
 
 
